4cca61ce24670253d3c92b43effbab67ed03255c
s1gr1d <sigrid.huemer@posteo.at>
2024-06-23 21:24:26 +0200

    change key to id

--- src/components/NonPerformantList.tsx
+++ src/components/NonPerformantList.tsx
@@ -52,8 +52,8 @@ export const NonPerformantList: FC = () => {
 			<ListInfo allItems={allItems} filteredItems={filteredItems} />
 
 			<ul>
-				{filteredItems.map((item, index) => (
-					<Item key={index} {...item} />
+				{filteredItems.map((item) => (
+					<Item key={item.id} {...item} />
 				))}
 			</ul>
 		</>
