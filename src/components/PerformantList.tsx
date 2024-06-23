c6c66e801aa6babdc56a3d53a645ae65d4f24a5c
s1gr1d <sigrid.huemer@posteo.at>
2024-06-23 22:23:05 +0200

    add SearchResults component

--- src/components/PerformantList.tsx
+++ src/components/PerformantList.tsx
@@ -1,36 +1,21 @@
-import { FC, useState, useEffect, memo, useMemo } from "react";
-import { dummyApi, DummyData } from "../utils/dummyApi";
+import { FC, useState, memo } from "react";
 import { useDebounce } from "../hooks/debounce.ts";
-import { Item, ItemProps } from "./Item.tsx";
 import { measureInteractionWithMark } from "../utils/measureInteraction.ts";
-import { ListInfo } from "./ListInfo.tsx";
+import { CounterButton } from "./CounterButton.tsx";
+import { SearchResults } from "./SearchResults.tsx";
 
-const MemoizedItem = memo(Item);
+const MemoizedSearchResults = memo(SearchResults);
 
 export const PerformantList: FC = () => {
-	const [allItems, setAllItems] = useState<ItemProps[]>([]);
 	const [searchTerm, setSearchTerm] = useState<string>("");
 
-	useEffect(() => {
-		dummyApi().then((data: unknown) => {
-			setAllItems(data as DummyData[]);
-		});
-	}, []);
-
 	const debouncedSearchTerm = useDebounce(searchTerm, 500);
-
-	const filteredItems = useMemo(() => {
-		const interaction = measureInteractionWithMark("Filter Items");
-		const filtered = allItems.filter((item) =>
-			item.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
-		);
-		requestAnimationFrame(() => setTimeout(() => interaction.end()));
-		return filtered;
-	}, [debouncedSearchTerm, allItems]);
 
 	return (
 		<>
 			<h1>Performant List</h1>
+
+			<CounterButton />
 
 			<input
 				type="text"
@@ -43,13 +28,7 @@ export const PerformantList: FC = () => {
 				}}
 			/>
 
-			<ListInfo allItems={allItems} filteredItems={filteredItems} />
-
-			<ul>
-				{filteredItems.map((item) => (
-					<MemoizedItem key={item.id} {...item} />
-				))}
-			</ul>
+			<MemoizedSearchResults debouncedSearchTerm={debouncedSearchTerm} />
 		</>
 	);
 };
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,15 +1,43 @@
-import { FC, memo } from "react";
-import { Item } from "./Item.tsx";
-import { DummyData } from "../utils/dummyApi.ts";
+import { FC, memo, useEffect, useMemo, useState } from "react";
+import { Item, ItemProps } from "./Item.tsx";
+import { dummyApi, DummyData } from "../utils/dummyApi.ts";
+import { measureInteractionWithMark } from "../utils/measureInteraction.ts";
+import { ListInfo } from "./ListInfo.tsx";
 
 const MemoizedItem = memo(Item);
 
-export const SearchResults: FC<{ items: DummyData[] }> = ({ items }) => {
+const filterItems = (allItems: ItemProps[], searchTerm: string) => {
+	const interaction = measureInteractionWithMark("Filter Items");
+	const filtered = allItems.filter((item) => {
+		return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+	});
+	requestAnimationFrame(() => setTimeout(() => interaction.end()));
+	return filtered;
+};
+
+export const SearchResults: FC<{ debouncedSearchTerm: string }> = ({
+	debouncedSearchTerm
+}) => {
+	const [allItems, setAllItems] = useState<ItemProps[]>([]);
+
+	useEffect(() => {
+		dummyApi().then((data: unknown) => {
+			setAllItems(data as DummyData[]);
+		});
+	}, []);
+
+	const filteredItems = useMemo(() => {
+		return filterItems(allItems, debouncedSearchTerm);
+	}, [debouncedSearchTerm, allItems]);
+
 	return (
-		<ul>
-			{items.map((item) => (
-				<MemoizedItem key={item.id} {...item} />
-			))}
-		</ul>
+		<>
+			<ListInfo allItems={allItems} filteredItems={filteredItems} />
+			<ul>
+				{filteredItems.map((item) => (
+					<MemoizedItem key={item.id} {...item} />
+				))}
+			</ul>
+		</>
 	);
 };
