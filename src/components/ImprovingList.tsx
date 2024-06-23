c3330d8161a25efc5277fc939794a3a711051c2d
s1gr1d <sigrid.huemer@posteo.at>
2024-06-23 22:30:13 +0200

    add child components with separate state

--- src/components/ImprovingList.tsx
+++ src/components/ImprovingList.tsx
@@ -1,49 +1,19 @@
-import { useState, useEffect, FC, useMemo, memo } from "react";
-import { dummyApi, DummyData } from "../utils/dummyApi";
-import { Item, ItemProps } from "./Item.tsx";
-import { ListInfo } from "./ListInfo.tsx";
+import { useState, FC } from "react";
 import { measureInteractionWithMark } from "../utils/measureInteraction.ts";
 import { useDebounce } from "../hooks/debounce.ts";
-
-const filterItems = (allItems: ItemProps[], searchTerm: string) => {
-	const interaction = measureInteractionWithMark("Filter Items");
-	const filtered = allItems.filter((item) => {
-		return item.name.toLowerCase().includes(searchTerm.toLowerCase());
-	});
-	interaction.end();
-	return filtered;
-};
-
-const MemoizedItem = memo(Item);
+import { SearchResults } from "./SearchResults.tsx";
+import { CounterButton } from "./CounterButton.tsx";
 
 export const ImprovingList: FC = () => {
-	const [allItems, setAllItems] = useState<ItemProps[]>([]);
 	const [searchTerm, setSearchTerm] = useState<string>("");
 
-	useEffect(() => {
-		dummyApi().then((data: unknown) => {
-			setAllItems(data as DummyData[]);
-		});
-	}, []);
-
 	const debouncedSearchTerm = useDebounce(searchTerm, 500);
-
-	const filteredItems = useMemo(() => {
-		return filterItems(allItems, debouncedSearchTerm);
-	}, [debouncedSearchTerm, allItems]);
-
-	// Counter Button State
-	const [counter, setCounter] = useState(0);
-
-	const handleClick = () => {
-		setCounter((prevCounter) => prevCounter + 1);
-	};
 
 	return (
 		<>
 			<h1>Improving List</h1>
 
-			<button onClick={handleClick}>Count {counter}</button>
+			<CounterButton />
 
 			<input
 				type="text"
@@ -56,13 +26,7 @@ export const ImprovingList: FC = () => {
 				}}
 			/>
 
-			<ListInfo allItems={allItems} filteredItems={filteredItems} />
-
-			<ul>
-				{filteredItems.map((item) => (
-					<MemoizedItem key={item.id} {...item} />
-				))}
-			</ul>
+			<SearchResults debouncedSearchTerm={debouncedSearchTerm} />
 		</>
 	);
 };
